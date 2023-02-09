f982fe76a980eaa385cabc45945062f3985923fe
Aleksandra Gracheva <alexapal@yandex.ru>
2023-02-09 13:43:55 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket=4000;
+        int mile=20;
+    int bonusMiles=ticket/mile;
+System.out.println("Начислено бонусных миль: " +bonusMiles);
+    }
+}
